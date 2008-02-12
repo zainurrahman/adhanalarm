682cbe6def6fdeb6794daee7545ff0175a1a48f6
imransomji <imransomji@5fb45585-f845-0410-9911-0f5a4794a2f1>
2008-02-12 21:50:34 +0000

    Started working on the settings UI, added array of different calculation methods.

--- AdhanAlarm/src/islam/adhanalarm/AdhanAlarm.java
+++ AdhanAlarm/src/islam/adhanalarm/AdhanAlarm.java
@@ -11,11 +11,11 @@ public class AdhanAlarm extends Activity {
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         setContentView(R.layout.main);
-        Spinner methods = (Spinner)findViewById(R.id.method);
-        methods.setAllowWrap(true);
-        ArrayAdapter adapter = ArrayAdapter.createFromResource(
+        Spinner calculation_methods = (Spinner)findViewById(R.id.calculation_methods);
+        calculation_methods.setAllowWrap(true);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
         		this, R.array.calculation_methods, android.R.layout.simple_spinner_item);
         		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        methods.setAdapter(adapter);
+        		calculation_methods.setAdapter(adapter);
     }
 }
