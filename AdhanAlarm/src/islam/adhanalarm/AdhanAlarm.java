c678b7f76a6dfd23673a021bd40726ba1ae8ab30
imransomji <imransomji@5fb45585-f845-0410-9911-0f5a4794a2f1>
2008-02-13 22:16:17 +0000

    Added more settings options to the user interface.

--- AdhanAlarm/src/islam/adhanalarm/AdhanAlarm.java
+++ AdhanAlarm/src/islam/adhanalarm/AdhanAlarm.java
@@ -11,9 +11,17 @@ public class AdhanAlarm extends Activity {
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         setContentView(R.layout.main);
+
+        Spinner notification_methods = (Spinner)findViewById(R.id.notification_methods);
+        notification_methods.setAllowWrap(true);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.notification_methods, android.R.layout.simple_spinner_item);
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                notification_methods.setAdapter(adapter);
+                
         Spinner calculation_methods = (Spinner)findViewById(R.id.calculation_methods);
         calculation_methods.setAllowWrap(true);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+        adapter = ArrayAdapter.createFromResource(
         		this, R.array.calculation_methods, android.R.layout.simple_spinner_item);
         		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         		calculation_methods.setAdapter(adapter);
