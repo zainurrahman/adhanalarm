231a809654a1f56a8a2ac125d160d45225685bbc
imransomji <imransomji@5fb45585-f845-0410-9911-0f5a4794a2f1>
2008-02-14 22:00:18 +0000

    Added more settings controls to the gui, made the prayer displayer a new tab and settings on a different tab to make things cleaner.  Having trouble figuring out how to get the GPS location, keep getting null pointer exception for some reason.

--- AdhanAlarm/src/islam/adhanalarm/AdhanAlarm.java
+++ AdhanAlarm/src/islam/adhanalarm/AdhanAlarm.java
@@ -1,9 +1,15 @@
 package islam.adhanalarm;
 
 import android.app.Activity;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TabHost;
+import android.view.View;
 
 public class AdhanAlarm extends Activity {
     /** Called when the activity is first created. */
@@ -18,12 +24,42 @@ public class AdhanAlarm extends Activity {
                 this, R.array.notification_methods, android.R.layout.simple_spinner_item);
                 adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                 notification_methods.setAdapter(adapter);
-                
+
         Spinner calculation_methods = (Spinner)findViewById(R.id.calculation_methods);
         calculation_methods.setAllowWrap(true);
         adapter = ArrayAdapter.createFromResource(
         		this, R.array.calculation_methods, android.R.layout.simple_spinner_item);
         		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         		calculation_methods.setAdapter(adapter);
+
+        TabHost tabs = (TabHost)this.findViewById(R.id.tabs);
+        tabs.setup();
+
+        TabHost.TabSpec one = tabs.newTabSpec("one");
+        one.setContent(R.id.content1);
+        one.setIndicator(getString(R.string.prayer_times), this.getResources().getDrawable(R.drawable.calendar));
+        tabs.addTab(one);
+
+        TabHost.TabSpec two = tabs.newTabSpec("two");
+        two.setContent(R.id.content2);
+        two.setIndicator(getString(R.string.settings), this.getResources().getDrawable(R.drawable.globe));
+        tabs.addTab(two);
+
+        tabs.setCurrentTab(1);
+
+        Button lookupGPS = (Button)findViewById(R.id.lookup_gps);
+        lookupGPS.setOnClickListener(new Button.OnClickListener() {  
+        	public void onClick(View v) {
+            	EditText latitude = (EditText)findViewById(R.id.latitude);
+            	EditText longitude = (EditText)findViewById(R.id.longitude);
+
+            	LocationManager locationMananager = (LocationManager)getSystemService(LOCATION_SERVICE);
+             	Location location = locationMananager.getCurrentLocation("gps");
+
+            	latitude.setText(Double.toString(location.getLatitude()));
+            	longitude.setText(Double.toString(location.getLongitude()));
+
+        	}  
+        });
     }
 }
