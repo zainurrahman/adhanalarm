8ccb29a7a1a614c73a10aca31e12257e16e95a20
imransomji <imransomji@5fb45585-f845-0410-9911-0f5a4794a2f1>
2009-08-21 21:34:15 +0000

    - Moved notification into NotificationService class, a step to non-modal notifications - Refactored classes into dialog, service and view packages

---
+++ trunk/AdhanAlarm/src/islam/adhanalarm/VARIABLE.java
@@ -0,0 +1,12 @@
+package islam.adhanalarm;
+
+import java.util.GregorianCalendar;
+
+
+public class VARIABLE {
+	public static GregorianCalendar[] schedule = new GregorianCalendar[7];
+	public static String[] TIME_NAMES;
+	private VARIABLE() {
+		// Private constructor to enforce un-instantiability.
+	}
+}
