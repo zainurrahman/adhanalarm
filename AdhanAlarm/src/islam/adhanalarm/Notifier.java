d172695f65bff7a52bbe8d9d240c9a77a135a3bc
imransomji <imransomji@5fb45585-f845-0410-9911-0f5a4794a2f1>
2009-08-30 16:29:28 +0000

    Made mediaPlayer remove the (Stop) from the end of the notification text when it has completed.

--- AdhanAlarm/src/islam/adhanalarm/Notifier.java
+++ AdhanAlarm/src/islam/adhanalarm/Notifier.java
@@ -15,21 +15,22 @@ public class Notifier {
 	
 	private static MediaPlayer mediaPlayer;
 	private static Context context;
+	private static Notification notification;
 	
 	public static void start(Context context, short timeIndex, long actualTime) {
 		Notifier.context = context;
-		stop();
-		
-		String notificationTitle = (timeIndex != CONSTANT.SUNRISE ? context.getString(R.string.allahu_akbar) + ": " : "") + context.getString(R.string.time_for) + " " + (timeIndex == CONSTANT.NEXT_FAJR ? context.getString(CONSTANT.TIME_NAMES[CONSTANT.FAJR]) : context.getString(CONSTANT.TIME_NAMES[timeIndex])).toLowerCase();
-		Notification notification = new Notification(R.drawable.icon, notificationTitle, actualTime);
-		
+		stopNotification();
+
+		notification = new Notification(R.drawable.icon, "", actualTime);
+		notification.tickerText = (timeIndex != CONSTANT.SUNRISE ? context.getString(R.string.allahu_akbar) + ": " : "") + context.getString(R.string.time_for) + " " + (timeIndex == CONSTANT.NEXT_FAJR ? context.getString(CONSTANT.TIME_NAMES[CONSTANT.FAJR]) : context.getString(CONSTANT.TIME_NAMES[timeIndex])).toLowerCase();
+
 		int notificationMethod = VARIABLE.settings.getInt("notificationMethodIndex", CONSTANT.DEFAULT_NOTIFICATION);
 		if(notificationMethod == CONSTANT.NO_NOTIFICATIONS || (timeIndex == CONSTANT.SUNRISE && !VARIABLE.alertSunrise())) return;
 		
 		int ringerMode = ((AudioManager)context.getSystemService(Context.AUDIO_SERVICE)).getRingerMode();
 		int callState = ((TelephonyManager)context.getSystemService(Context.TELEPHONY_SERVICE)).getCallState();
 		if(notificationMethod == CONSTANT.RECITE_ADHAN && ringerMode != AudioManager.RINGER_MODE_SILENT && ringerMode != AudioManager.RINGER_MODE_VIBRATE && callState == TelephonyManager.CALL_STATE_IDLE) {
-			notificationTitle += " (" + context.getString(R.string.stop) + ")";
+			notification.tickerText = notification.tickerText + " (" + context.getString(R.string.stop) + ")";
 			int alarm = R.raw.beep;
 			if(timeIndex == CONSTANT.DHUHR || timeIndex == CONSTANT.ASR || timeIndex == CONSTANT.MAGHRIB || timeIndex == CONSTANT.ISHAA) {
 				alarm = R.raw.adhan;
@@ -40,20 +41,34 @@ public class Notifier {
 			mediaPlayer.setScreenOnWhilePlaying(true);
 			mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
 				public void onCompletion(MediaPlayer mp) {
-					WakeLock.release();
+					notification.tickerText = notification.tickerText.toString().replace(" (" + Notifier.context.getString(R.string.stop) + ")", "");
+					notification.defaults = 0;
+					startNotification();
 				}
 			});
 			try {
 				mediaPlayer.start();
 			} catch(Exception ex) {
-				notificationTitle += " - " + context.getString(R.string.error_playing_alert);
+				notification.tickerText = notification.tickerText + " - " + context.getString(R.string.error_playing_alert);
 			}
 			notification.defaults = Notification.DEFAULT_VIBRATE | Notification.DEFAULT_LIGHTS;
 		} else {
 			notification.defaults = Notification.DEFAULT_ALL;
 		}
+		startNotification();
+	}
+	public static void stop() {
+		stopNotification();
+		WakeLock.release();
+	}
+	
+	private static void stopNotification() {
+		if(mediaPlayer != null && mediaPlayer.isPlaying()) mediaPlayer.stop();
+		if(context != null) ((NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE)).cancelAll();
+	}
+	private static void startNotification() {
 		Intent i = new Intent(context, AdhanAlarm.class);
-		notification.setLatestEventInfo(context, context.getString(R.string.app_name), notificationTitle, PendingIntent.getActivity(context, 0, i, PendingIntent.FLAG_CANCEL_CURRENT | PendingIntent.FLAG_ONE_SHOT));
+		notification.setLatestEventInfo(context, context.getString(R.string.app_name), notification.tickerText, PendingIntent.getActivity(context, 0, i, PendingIntent.FLAG_CANCEL_CURRENT | PendingIntent.FLAG_ONE_SHOT));
 		notification.contentIntent = PendingIntent.getBroadcast(context, 0, new Intent(context, ClickNotificationReceiver.class), PendingIntent.FLAG_CANCEL_CURRENT | PendingIntent.FLAG_ONE_SHOT);
 		notification.deleteIntent = PendingIntent.getBroadcast(context, 0, new Intent(context, ClearNotificationReceiver.class), PendingIntent.FLAG_CANCEL_CURRENT | PendingIntent.FLAG_ONE_SHOT);
 		((NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE)).notify(1, notification);
@@ -65,10 +80,5 @@ public class Notifier {
 			}
 			WakeLock.release();
 		}
-	}
-	
-	public static void stop() {
-		if(mediaPlayer != null && mediaPlayer.isPlaying()) mediaPlayer.stop();
-		if(context != null) ((NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE)).cancelAll();
 	}
 }
